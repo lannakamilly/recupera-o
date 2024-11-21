ce8dec96f9cc13cbb5ef98ab3364590e3aa05cee
flaviaglenda <flaviaglenda15@gmail.com>
2024-11-21 08:12:30 -0300

    fla

---
+++ ex1.py
@@ -0,0 +1,23 @@
+# Flávia Glenda e Lanna Kamilly 
+
+def questao_teste_mesa (n1, n2):
+
+while n1 != n2:
+
+
+if (n1n2): n2 = n2 - n1
+
+
+
+else: n1 = n1 return n1 - n2
+
+
+
+ print(questao_teste_mesa (40, 5))
+
+
+
+
+Página 1
+
+
